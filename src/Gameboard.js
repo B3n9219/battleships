c4bb7bd994c50ce6344cb7c75f9fa7ffef5b9b32
B3n9219 <benkirk1441@gmail.com>
2025-08-04 11:38:20 +0100

    Created the gameboard object

---
+++ src/Gameboard.js
@@ -0,0 +1,83 @@
+import { Ship } from "./Ship.js";
+
+export class Gameboard {
+    constructor(size = 10) {
+        this.size = size
+        this.ships = []
+        this.missedAttacks = []
+    }
+    placeShip(cords) {
+        if (cords.length < 2) {
+            throw new Error("Ship must be at least 2 long")
+        }
+        if (!(this.cordsValid(cords))) {
+            throw new Error("Ship cords are not valid")
+        }
+        if (!(this.cordsFree(cords))) {
+            throw new Error("Trying to place new ship in occupied area")
+        }
+        const ship = new Ship(cords.length)
+        ship.place(cords)
+        this.ships.push(ship)
+    }
+    cordsValid(cords) {
+        let axis, cross
+        if (cords[0].x !== cords[1].x) {
+            axis = "x"
+            cross = "y"
+        } else {
+            axis = "y"
+            cross = "x"
+        }
+        for (let i = 1; i < cords.length; i++) {
+            if (Math.abs(cords[i][axis] - cords[i-1][axis]) !== 1) {
+                return false
+            }
+            if (cords[i-1][cross] !== cords[i][cross]) {
+                return false
+            }
+        }
+        return true
+    }
+    cordsFree(cords) {
+        for (let cord of cords) {
+            if (!(this.cordOnBoard(cord))) {
+                return false
+            }
+            for (let ship of this.ships) {
+                for (let shipCord of ship.cords) {
+                    if (cord.x === shipCord.x && cord.y === shipCord.y) {
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
+    cordOnBoard(cord) {
+        return cord.x >= 0 && cord.x < this.size && cord.y >= 0 && cord.y < this.size;
+    }
+    recieveAttack(cord) {
+        for (let ship of this.ships) {
+            for (let shipCord of ship.cords) {
+                if (shipCord.x === cord.x && shipCord.y === cord.y) {
+                    ship.hit(cord)
+                    return
+                }
+            }
+        }
+        if (!this.cordOnBoard(cord)){
+            throw new Error("Attack cord is not on the board (too big or small)")
+        } else {
+            this.missedAttacks.push(cord)
+        }
+    }
+    areAllShipsSunk() {
+        for (let ship of this.ships) {
+            if (!ship.isSunk()) {
+                return false
+            }
+        }
+        return true
+    }
+}
---
+++ src/Gameboard.test.js
@@ -0,0 +1,121 @@
+import { Gameboard } from "./Gameboard";
+
+describe("Gameboard.placeShip()", () => {
+    describe("Valid placements", () => {
+        test("horizontal ship", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 0, y: 0}, {x: 1, y: 0}])
+            expect(gameboard.ships.length).toBe(1)
+        })
+        test("vertical ship", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 0, y: 0}, {x: 0, y: 1}])
+            expect(gameboard.ships.length).toBe(1)
+        })
+        test("long ship", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3}])
+            expect(gameboard.ships.length).toBe(1)
+        })
+        test("ship on board edge", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 9, y: 9}, {x:9, y: 8}])
+            expect(gameboard.ships.length).toBe(1)
+        })
+        test("cords backwards", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 1, y: 0}, {x:0, y: 0}])
+            expect(gameboard.ships.length).toBe(1)
+        })
+        test("consecutive ships", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 0, y: 0}, {x:1, y: 0}])
+            gameboard.placeShip([{x: 2, y: 0}, {x:3, y: 0}])
+            expect(gameboard.ships.length).toBe(2)
+        })
+    })
+
+    describe("Invalid placements", () => {
+        test("ship cords < 2", () => {
+            const gameboard = new Gameboard()
+            expect(() => gameboard.placeShip([{x: 0, y: 0}])).toThrow("Ship must be at least 2 long")
+        })
+        test("invalid ship placement (broken cords)", () => {
+            const gameboard = new Gameboard()
+            expect(() => gameboard.placeShip([{x: 0, y: 0}, {x: 2, y: 0}])).toThrow("Ship cords are not valid")
+
+        })
+        test("invalid ship placement (diagonal cords)", () => {
+            const gameboard = new Gameboard()
+            expect(() => gameboard.placeShip([{x: 0, y: 0}, {x: 1, y: 1}])).toThrow("Ship cords are not valid")
+        })
+        test("invalid ship placement (out of bounds)", () => {
+            const gameboard = new Gameboard()
+            expect(() => gameboard.placeShip([{x: 10, y: 10}, {x: 9, y: 10}])).toThrow("Trying to place new ship in occupied area")
+        })
+        test("Ship overlaps with other ship", () => {
+            const gameboard = new Gameboard()
+            gameboard.placeShip([{x: 0, y: 0}, {x: 1, y: 0}])
+            expect(() => gameboard.placeShip([{x: 1, y: 0}, {x: 2, y: 0}])).toThrow("Trying to place new ship in occupied area")
+        })
+    })
+})
+
+
+describe("Gameboard.recieveAttack", () => {
+     test("Miss on empty board", () => {
+         const gameboard = new Gameboard()
+         gameboard.recieveAttack({x: 5, y: 5})
+         expect(gameboard.missedAttacks).toEqual([{x: 5, y: 5}])
+     })
+    test("Miss (with ship on board)", () => {
+        const gameboard = new Gameboard()
+        gameboard.placeShip([{x: 0, y: 0}, {x: 1, y: 0}])
+        gameboard.recieveAttack({x: 5, y: 5})
+        expect(gameboard.missedAttacks).toEqual([{x: 5, y: 5}])
+    })
+    test("Miss (on border)", () => {
+        const gameboard = new Gameboard()
+        gameboard.recieveAttack({x: 9, y: 9})
+        expect(gameboard.missedAttacks).toEqual([{x: 9, y: 9}])
+    })
+    test("Ship hit", () => {
+        const gameboard = new Gameboard()
+        gameboard.placeShip([{x: 5, y: 5}, {x: 6, y: 5}])
+        gameboard.recieveAttack({x: 5, y: 5})
+        expect(gameboard.missedAttacks).toEqual([])
+    })
+    test("Attack out of bounds", () => {
+        const gameboard = new Gameboard()
+        expect(() => gameboard.recieveAttack({x: 10, y: 10})).toThrow("Attack cord is not on the board (too big or small)")
+    })
+})
+
+describe("Gameboard.areAllShipsSunk", () => {
+    test("No ships sunk", () => {
+        const gameboard = new Gameboard()
+        gameboard.placeShip([{x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}])
+        gameboard.placeShip([{x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}])
+        expect(gameboard.areAllShipsSunk()).toBe(false)
+    })
+    test("1 / 2 ships sunk", () => {
+        const gameboard = new Gameboard()
+        gameboard.placeShip([{x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}])
+        gameboard.placeShip([{x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}])
+        for (let i = 1; i < 4; i++) {
+            gameboard.recieveAttack({x: i, y: 0})
+        }
+        expect(gameboard.areAllShipsSunk()).toBe(false)
+    })
+    test("all ships sunk", () => {
+        const gameboard = new Gameboard()
+        gameboard.placeShip([{x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}])
+        gameboard.placeShip([{x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}])
+        for (let i = 1; i < 4; i++) {
+            gameboard.recieveAttack({x: i, y: 0})
+            gameboard.recieveAttack({x: i, y: 1})
+
+        }
+        expect(gameboard.areAllShipsSunk()).toBe(true)
+    })
+})
